9ebc5bffd97122ed6e02621279a867ca504af3a1
Andrés Estévez <aecostas@gmail.com>
2019-11-16 17:26:21 +0100

    Add logger

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const parse = require('csv-parse')
 const axios = require('axios');
+const parse = require('csv-parse');
+const winston = require('winston');
 
 const url = 'https://abertos.xunta.gal/catalogo/cultura-ocio-deporte/-/dataset/0401/praias-galegas-con-bandeira-azul-2019/001/descarga-directa-ficheiro.csv';
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console({format: winston.format.simple()})
+  ]
+});
+
+logger.log({
+  level: 'info',
+  message: 'Starting points of interest application!'
+});
+
+logger.log({
+  level: 'debug',
+  message: 'debugging message'
+});
 
 axios.get(url).then( (response) => {
 
@@ -12,7 +34,7 @@ axios.get(url).then( (response) => {
     columns: true
   },
   function(err, result) {
-    console.log(result);
+    console.log(result.length);
   })
 
 });
